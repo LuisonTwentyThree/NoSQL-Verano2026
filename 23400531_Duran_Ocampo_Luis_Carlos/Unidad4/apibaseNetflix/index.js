b4de4d42f2f23c7a866fe6907bd6f8e8ccc249a6
LuisonTwentyThree <lucaduranoc@ittepic.edu.mx>
2026-07-21 14:20:40 -0700

    Update index.js

--- 23400531_Duran_Ocampo_Luis_Carlos/Unidad4/apibaseNetflix/index.js
+++ 23400531_Duran_Ocampo_Luis_Carlos/Unidad4/apibaseNetflix/index.js
@@ -3,7 +3,8 @@ const morgan = require('morgan');
 const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
-
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
